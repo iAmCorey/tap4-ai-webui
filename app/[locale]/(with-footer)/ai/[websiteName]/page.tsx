97fc13c8a04e9ec59d226b3e5614c492bdbbcdd4
Corey Chiu <qiuyu@qiuyu.dev>
2024-07-18 19:27:00 +0800

    update ui

--- app/[locale]/(with-footer)/ai/[websiteName]/page.tsx
+++ app/[locale]/(with-footer)/ai/[websiteName]/page.tsx
@@ -6,6 +6,7 @@ import { getTranslations } from 'next-intl/server';
 
 import { Separator } from '@/components/ui/separator';
 import BaseImage from '@/components/image/BaseImage';
+import Icon from '@/components/image/Icon';
 import MarkdownProse from '@/components/MarkdownProse';
 
 export async function generateMetadata({
@@ -49,16 +50,35 @@ export default async function Page({ params: { websiteName } }: { params: { webs
   }
   const data = dataList[0];
 
+  const formatDate = (dateString: string): string => {
+    const date = new Date(dateString);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // getMonth() returns month from 0 to 11
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+  };
+
+  const formattedDate = formatDate(data.create_time);
+
   return (
     <div className='w-full'>
-      <div className='flex flex-col px-6 py-5 lg:h-[403px] lg:flex-row lg:justify-between lg:px-0 lg:py-10'>
+      <div className='flex flex-col px-6 py-5 lg:h-[433px] lg:flex-row lg:justify-between lg:px-0 lg:py-10'>
         <div className='flex flex-col items-center lg:items-start'>
+          <div className='flex flex-row items-center pb-6'>
+            <div className='font-semibold '>{t('home')}</div>
+            <div className=''>
+              <Icon src='/icons/arrow.svg' className='h-[28px] w-[28px]' />
+            </div>
+            <div className='text-gray-500'>{data.title}</div>
+          </div>
           <div className='space-y-1 text-balance lg:space-y-3'>
-            <h1 className='text-2xl font-bold text-gray-700 lg:text-5xl'>{data.title}</h1>
-            <div className='flex h-fit w-fit items-center justify-center gap-[2px] whitespace-nowrap pb-5 '>
+            <h1 className='text-2xl font-bold text-gray-700 lg:text-4xl'>{data.title}</h1>
+            <div className='flex h-fit w-fit items-center justify-center gap-[2px] whitespace-nowrap pb-5 pt-2'>
               {data.tags_list.map((item) => (
                 <p
-                  className='bottom-1 mx-1 w-fit rounded-xl border border-orange-200 bg-orange-50  px-2 py-1 text-sm shadow-sm hover:border-orange-300 hover:bg-orange-400 hover:text-white'
+                  className='bottom-1 mr-2 w-fit rounded-lg border border-orange-200 bg-orange-50  px-2 py-1 text-sm shadow-sm hover:border-orange-300 hover:bg-orange-400 hover:text-white'
                   key={item}
                 >
                   {item}
@@ -66,6 +86,10 @@ export default async function Page({ params: { websiteName } }: { params: { webs
               ))}
             </div>
             <h2 className='text-xs text-slate-600 lg:text-sm'>{data.description}</h2>
+          </div>
+          <div className='flex flex-row pt-5'>
+            <div className='text-sm font-semibold'>{t('added-on')}:</div>
+            <div className='px-2 text-sm'>{formattedDate}</div>
           </div>
           <a
             href={data.url}
